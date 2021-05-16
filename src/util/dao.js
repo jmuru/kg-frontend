4524ef27e01e64df68b2aa708cdc9e8021b8dae3
jmuru <jmuruguru94@gmail.com>
2021-05-16 14:48:22 -0400

    update ep

--- src/util/dao.js
+++ src/util/dao.js
@@ -2,16 +2,23 @@ import axios from 'axios';
 
 const SERVER_URL_BASE = "";
 
-export function postAccessoryData (placement, subType, aData) {
-    const url = `/api/accessory/create`
-    if (window.confirm(`Are you sure you eant to save this accessory as sub type ${subType} and placement ${placement}`)) {
-        axios.post(url,{
-            placement: placement,
-            subType: subType,
-            accessory: aData
-        });
-    }
 
+
+export async function postAccessoryData (placement, subType, aData) {
+    const url = `/api/accessory/create`;
+    if (window.confirm(`Are you sure you eant to save this accessory as sub type ${subType} and placement ${placement}`)) {
+       const response = await axios.post(url, {
+           headers: {
+               'Content-Type': 'application/json',
+           },
+           data: {
+               placement: placement,
+               subType: subType,
+               accessory: aData
+           }
+       });
+        console.log("create kat accessory response", response.status);
+    }
 }
 
 export async function getKatData () {
