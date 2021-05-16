ede9670a741bac5e023de93b40de9f5cc2201eb9
jmuru <jmuruguru94@gmail.com>
2021-05-16 12:27:06 -0400

    headers

--- src/util/dao.js
+++ src/util/dao.js
@@ -2,6 +2,10 @@ import axios from 'axios';
 
 const SERVER_URL_BASE = "https://polar-earth-91730.herokuapp.com";
 
+const headers = {
+    "Access-Control-Allow-Origin": "*"
+}
+
 export function postAccessoryData (placement, subType, aData) {
     const url = `${SERVER_URL_BASE}/accessory/create`
     if (window.confirm(`Are you sure you eant to save this accessory as sub type ${subType} and placement ${placement}`)) {
@@ -16,7 +20,7 @@ export function postAccessoryData (placement, subType, aData) {
 
 export async function getKatData () {
     const url = `${SERVER_URL_BASE}/generate/kat`;
-    const response = await axios.get(url);
+    const response = await axios.get(url, headers);
     console.log("generate cat response status", response.status);
     return response.data;
 }
